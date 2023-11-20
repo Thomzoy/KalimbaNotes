4d8f1ee0909252a6ecd3fc47a4eb8257f56c2315
Thomas Petit-Jean <thomas.petitjean@aphp.fr>
2023-11-21 00:15:58 +0100

    autoplay

--- js/markers.js
+++ js/markers.js
@@ -28,13 +28,17 @@ class MarkerManager {
 
         // add click event
         marker.on('click', function() {
+
             var audio = document.getElementById('audioPlayer');
-            if (!audio.paused){
+            if ((!audio.paused) && (audio.src.split('/').pop() == marker.mp3.split('/').pop())){
+                // We check if we are on the same marker
+                console.log("pausing");
                 audio.pause();
                 audio.currentTime = 0; // Reset audio to the beginning
             }
             else {
                 if (marker.marker_type == "active"){
+                    console.log("playing");
                     audio.src = marker.mp3;
                     if (audio.paused) {
                         audio.play();
@@ -52,22 +56,27 @@ class MarkerManager {
     }
 
     updateMarker({marker, marker_type = null}) {
-        if (marker_type == "active") {
+        if ((marker_type == "active") && (marker.marker_type != "active"))  {
             // Check if previous markers were seen
+            console.log("case 1");
             if ((marker.idx > 1) && !(this.markers[marker.idx-2].was_seen)) {
                 return
             }
             var color = "green";
             marker.marker_type = marker_type;
-            marker.was_seen = true;
             var icon = new L.AwesomeNumberMarkers({
                 number: marker.idx, 
                 markerColor: color,
             });
             marker.setIcon(icon);
+
+            // If not already playing, play the audio:
+            marker.fireEvent('click');
+            marker.was_seen = true;
         }
-        if (marker_type == "inactive") {
-            var color = "lightgray";
+        if ((marker_type == "inactive") && (marker.marker_type != "inactive")){
+            console.log("case 2");
+            var color = (marker.was_seen) ? 'blue' : 'lightgray';
             marker.marker_type = marker_type;
             var icon = new L.AwesomeNumberMarkers({
                 number: marker.idx, 
