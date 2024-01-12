a221964f869985c95086f9102c2ce2dd451c5c53
Thomas Petit-Jean <30775613+Thomzoy@users.noreply.github.com>
2024-01-12 19:41:30 +0000

    continue

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function enableScroll(){
     document.body.removeEventListener('touchmove', preventDefault);
 }
 
-disableScroll()
+//disableScroll()
 
 const IndexToNoteTable = [0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37]
-const DELAY = 50 / 2 // Approx. 500ms per 40 frames in KalimbaGo
+const DELAY = 50 / 4 // Approx. 500ms per 40 frames in KalimbaGo
 
 function createNote(noteId, noteDelay){
 
@@ -23,6 +23,47 @@ function createNote(noteId, noteDelay){
     element.setAttribute("id", "my-square");
     element.style.animationDelay = noteDelay + 'ms';
     element.style.webkitAnimationDelay = noteDelay + 'ms';
+
+    element.style.animationDuration = "500ms"
+    element.style.webkitAnimationDuration = "500ms"
+
+
+    let options = {
+        root: rect,
+        rootMargin: "0px",
+        threshold: 1.0,
+    };
+      
+    let callback = (entries, observer) => {
+
+        function changeClassForTime(time) {
+            // Add the new class
+            rect.classList.remove('rectangle-inside');
+            rect.classList.add('rectangle-outside');
+        
+            // Set a timeout to remove the class after the specified time
+            setTimeout(() => {
+                rect.classList.add('rectangle-inside');
+                rect.classList.remove('rectangle-outside');
+            }, time);
+        }
+
+      const isIntersecting = entries[0].isIntersecting;
+      if (!isIntersecting) {
+        //element.style.opacity = 0;
+        changeClassForTime(500);
+        //observer.root.style.background = "linear-gradient(to bottom, #abbcc7, #ffffff);";
+      } else {
+        //element.style.opacity = 1;
+        // rect.classList.add('rectangle-inside');
+        // rect.classList.remove('rectangle-outside');
+        // observer.root.style.background = "linear-gradient(to bottom, #abbcc7, red);";
+      }
+      console.log(element.classList)
+    }
+    let observer = new IntersectionObserver(callback, options);
+  
+    observer.observe(element);
 
     return [element, rect]
 }
